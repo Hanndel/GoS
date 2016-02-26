d132c9ab2140e73837ee3fef343f4d49166eff83
Hanndel <lnote@outlook.es>
2016-02-26 13:50:46 +0100

    Update CC.lua

--- Common/CC.lua
+++ Common/CC.lua
@@ -5,20 +5,20 @@ function IsCCable(Unit)
 	if NoCCables[GetNetworkID(Unit)] ~= CCable then
 		return true
 	end
-
-	OnUpdateBuff(function(Object,buffProc)
-		if GetNetWorkID(Object) == CCable then
-			if buffProc.Type == 4 or buffProc.Type == 15 or buffProc.Type == 17 then
-				table.insert(NoCCables, GetNetworkID(Object))
-			end
-		end
-	end)
-
-	OnRemoveBuff(function(Object,buffProc)
-		if GetNetWorkID(Object) == CCable then
-			if buffProc.Type == 4 or buffProc.Type == 15 or buffProc.Type == 17 then
-				table.remove(NoCCables, GetNetworkID(Object))
-			end
-		end
-	end)
 end
+
+OnUpdateBuff(function(Object,buffProc)
+	if GetNetWorkID(Object) == CCable then
+		if buffProc.Type == 4 or buffProc.Type == 15 or buffProc.Type == 17 then
+			table.insert(NoCCables, GetNetworkID(Object))
+		end
+	end
+end)
+
+OnRemoveBuff(function(Object,buffProc)
+	if GetNetWorkID(Object) == CCable then
+		if buffProc.Type == 4 or buffProc.Type == 15 or buffProc.Type == 17 then
+			table.remove(NoCCables, GetNetworkID(Object))
+		end
+	end
+end)
